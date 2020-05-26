fc62e4b555d111a4823980c043b84a32736247c7
llaulier <lioraloche@yahoo.fr>
2020-05-26 22:31:38 +0200

    modify my contacts

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -7,43 +7,43 @@ const users = [
     id: "0",
     name: "Robert Reyes",
     avatar: "https://randomuser.me/api/portraits/men/28.jpg",
-    online: false
+    online: false,
   },
   {
     id: "1",
     name: "Nellie Caldwell",
     avatar: "https://randomuser.me/api/portraits/women/17.jpg",
-    online: false
+    online: false,
   },
   {
     id: "2",
     name: "Vernon Mason",
     avatar: "https://randomuser.me/api/portraits/men/84.jpg",
-    online: true
+    online: true,
   },
   {
     id: "3",
     name: "Erica Hunt",
     avatar: "https://randomuser.me/api/portraits/women/87.jpg",
-    online: false
+    online: false,
   },
   {
     id: "4",
     name: "Juanita Phillips",
     avatar: "https://randomuser.me/api/portraits/women/55.jpg",
-    online: true
+    online: true,
   },
   {
     id: "5",
     name: "Olivia Witendhal",
     avatar: "https://randomuser.me/api/portraits/women/77.jpg",
-    online: true
-  }
+    online: true,
+  },
 ];
 const ContactList = () => (
   <div>
-    <h3>Your contacts</h3>
-    {users.map(item => (
+    <h3>My contacts</h3>
+    {users.map((item) => (
       <Contact
         key={item.id}
         name={item.name}
