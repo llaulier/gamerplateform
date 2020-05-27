af8711fcb47507d8d5af59b281f9be005e7275a9
llaulier <lioraloche@yahoo.fr>
2020-05-27 09:05:17 +0200

    menu html

---
+++ src/components/Menu.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+return (
+  <div class="container">
+    <aside>
+      <div class="menu">
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4E03AQHgKaLf34VqVg/profile-displayphoto-shrink_200_200/0?e=1594252800&v=beta&t=p4sXfGehISolWyrVKUqd7bzUMinCwajiephHNjVcCIE"
+          alt="My Pic"
+        />
+        <div class="menu__info">
+          <h4>
+            <img src="/twitch.png" alt="twitch logo" />
+          </h4>
+        </div>
+      </div>
+      <div id="navbar">
+        <nav>
+          <ul>
+            <a>
+              <li>
+                <img />
+                <span></span>
+              </li>
+            </a>
+            <a>
+              <li>
+                <img />
+                <span></span>
+              </li>
+            </a>
+          </ul>
+        </nav>
+      </div>
+    </aside>
+  </div>
+);
